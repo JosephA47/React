c788c1237f9d5a2e550e3657275b3792fae9bcc6
Joseph <jandrade008@gmail.com>
2019-12-16 15:05:27 -0800

    moved files around

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import NavBar from './components/NavBar';
+//import './App.css';
+
+function App() {
+  return (
+    <Wrapper>
+      <Header/>
+      <NavBar/>
+    </Wrapper>
+  );
+}
+
+export default App;
